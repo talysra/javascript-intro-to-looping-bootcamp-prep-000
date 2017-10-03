8b516b7b00d1bfba9cb3973131b99d210c4c0043
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-03 22:01:47 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,9 +1,9 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
     if (i === 1) {
-      array.push("I am 1 strange loop.");
+      array.push(`I am 1 strange loop.`);
     } else {
-      array.push("I am ${i} strange loops.");
+      array.push(`I am ${i} strange loops.`);
   }
 
   return array
@@ -13,7 +13,7 @@ function whileLoop(n) {
   while (n > 0) {
     console.log(--n)
   }
-  
+
   return "done";
 }
 
