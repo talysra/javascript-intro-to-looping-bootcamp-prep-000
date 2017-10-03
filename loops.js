5ffbf9d0f1a6f1bf5ec67f38c5ffc2c21ccbd200
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-03 22:00:16 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,11 +1,11 @@
 function forLoop(array) {
   for (let i = 0; i < 25; i++) {
-    if (i == 1) {
+    if (i === 1) {
       array.push("I am 1 strange loop.");
-    }
-    else {
+    } else {
       array.push("I am ${i} strange loops.");
   }
+
   return array
 }
 
@@ -13,9 +13,10 @@ function whileLoop(n) {
   while (n > 0) {
     console.log(--n)
   }
+  
   return "done";
 }
 
 function doWhileLoop(array) {
-  
+
 }
