7894e44989b0f1cd4cf7e125487ada2cab9bc183
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-03 21:26:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -1,3 +1,5 @@
-for (var i = 1; i < 100; i++) {
-  console.log( "Hello World the " + i + " time" );
+function forLoop(array) {
+  for (let i = 0; i < 25; i++) {
+    console.log("I am ${i} strange loops.");
+  }
 }
