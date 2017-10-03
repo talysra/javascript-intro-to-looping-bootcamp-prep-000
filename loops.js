a49c7cd20b0ecf7672c0234475a29c2f38a04dac
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-03 22:03:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,4 +19,11 @@ function whileLoop(n) {
 
 function doWhileLoop(array) {
   
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+
+do {
+  console.log('doo-bee-doo-bee-doo')
+} while (maybeTrue());
 }
