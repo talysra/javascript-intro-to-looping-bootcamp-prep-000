548725e70cacc13c4e6a5d301ba1052fa3bbbe2c
Stanislava Koklina <medula_oblomgata@mail.ru>
2017-10-03 22:05:07 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -15,15 +15,16 @@ function whileLoop(n) {
   }
 
   return "done";
-}
 
 function doWhileLoop(array) {
-  
+
   function maybeTrue() {
   return Math.random() >= 0.5
 }
 
 do {
-  console.log('doo-bee-doo-bee-doo')
-} while (maybeTrue());
+  array = array.slice(1)
+} while (array.length > 0 && maybeTrue);
+}
+
 }
